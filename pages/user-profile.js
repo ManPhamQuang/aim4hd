e8affa033e7187cdfc6e6c6ba71fae558666c8d1
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2020-11-25 13:48:15 +0700

    leftside user info update 1

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -1,12 +1,33 @@
+//* Components import 
 import Head from "next/head";
-import { Grid } from "@material-ui/core";
 import Posts from "../components/Posts";
 import Filter from "../components/Filter";
 import axios from 'axios'
 import React, {useState, useEffect} from 'react'
+//* Styling import 
 import Avatar from '@material-ui/core/Avatar';
+import { Grid } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';  
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+          "justify": 'center'
+      }
+    },
+    small: {
+        width: theme.spacing(20),
+        height: theme.spacing(20),
+        margin: theme.spacing(3)
+    }
+    
+  }));
+
 
 export default function UserProfile() {
+    const classes = useStyles();
     const [user, setUser] = useState({})
     useEffect(() => {
         axios.get("https://aim4hd.herokuapp.com/api/v1/users/5fab4912ffd1131f3cace691")
@@ -15,13 +36,18 @@ export default function UserProfile() {
     })
     return (
         <Grid container justify="center">
-            <Grid item xs={12} md={3}>
-            <Avatar alt="Remy Sharp" src={user.avatar} />
-                {/* Leftside user info */}
+            <Grid item xs={12} md={3} >
+            {/* Leftside user info */}
+            <div className={classes.root}>
+            <Avatar alt="User avatar" src={user.avatar} className={classes.small} />
+            <Typography variant="h3" align="center">{user.name}</Typography>
+            </div>
+            
+            
+            
             </Grid>
             <Grid item xs={11} md={5}>
                 {/* center user description */}
-                <h1>{user.name}</h1>
             </Grid>
             <Grid item xs={false} md={4}>
                 {/* suggested users - for later */}

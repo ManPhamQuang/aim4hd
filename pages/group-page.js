56da7e5ebef8b83292c95f91d899797c4cdda308
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2020-12-09 11:55:21 +0700

    Group Page created

---
+++ pages/group-page.js
@@ -0,0 +1,35 @@
+//* Components import
+import Head from "next/head";
+import Posts from "../components/Posts";
+import Filter from "../components/Filter";
+import LeftGroup from "../components/LeftGroup";
+import CenterGroup from "../components/CenterGroup";
+//* Styling import
+import { Grid } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function UserProfile() {
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Group name | AimforHD</title>
+                <meta
+                    name="viewport"
+                    content="minimum-scale=1, initial-scale=1, width=device-width"
+                />
+            </Head>
+            <Grid container justify="center">
+                <Grid item xs={11} md={3}>
+                    {/* <LeftProfile user={user} /> */}
+                </Grid>
+                <Grid item xs={11} md={5}>
+                    {/* <CenterProfile user={user} /> */}
+                </Grid>
+                <Grid item xs={false} md={4}>
+                    {/* suggested users - for later */}
+                </Grid>
+            </Grid>
+        </React.Fragment>
+    );
+}

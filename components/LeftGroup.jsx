97c05d285e025febd67f71b64748c46349715cd9
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2020-12-09 18:06:02 +0700

    left group update 1

--- components/LeftGroup.jsx
+++ components/LeftGroup.jsx
@@ -5,17 +5,96 @@ import Filter from "../components/Filter";
 import axios from "axios";
 import React, { useState, useEffect } from "react";
 import Typography from "@material-ui/core/Typography";
+import Avatar from "@material-ui/core/Avatar";
+import { Grid, Hidden } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		display: "flex",
+		margin: theme.spacing(2),
+		justifyContent: "center",
+	},
+	container: {
+		[theme.breakpoints.down("sm")]: {
+			borderRadius: "8px",
+			backgroundColor: "#ffffff",
+			paddingTop: "0.5rem",
+			paddingBottom: "0.5rem",
+			marginBottom: "1.5rem",
+		},
+		[theme.breakpoints.up("md")]: {
+			marginLeft: "1rem",
+			marginRight: "1rem",
+		},
+	},
+	profile_picture: {
+		width: theme.spacing(23),
+		height: theme.spacing(23),
+	},
+	logo: {
+		fontSize: "3rem",
+	},
+	logo2: {
+		fontSize: "2rem",
+	},
+	info: {
+		fontSize: "1rem",
+	},
+	contactInfo: {
+		display: "flex",
+		marginLeft: "15%",
+		marginBottom: "2%",
+		marginTop: "2%",
+	},
+}));
+const ColorLine = ({ color }) => (
+	<hr
+		style={{
+			borderStyle: "none",
+			backgroundColor: color,
+			height: 2.5,
+			width: "100%",
+		}}
+	/>
+);
 
 export default function LeftGroup() {
-    return (
-        <div>
-            <Typography
-                variant="h5"
-                align="center"
-                style={{ fontWeight: "bold" }}
-            >
-                Hello
-            </Typography>
-        </div>
-    );
+	const classes = useStyles();
+	return (
+		<div className={classes.container}>
+			<div className={classes.root}>
+				<Avatar alt="Group avatar" className={classes.profile_picture}>
+					N
+				</Avatar>
+			</div>
+			<div className={classes.root}>
+				<Typography variant="h5" align="center" style={{ fontWeight: "bold" }}>
+					Group name
+				</Typography>
+			</div>
+			<div className={classes.root}>
+				<Typography variant="h6" align="center">
+					Course name
+				</Typography>
+			</div>
+			<Hidden mdUp>
+				<ColorLine color="#f2f2f2" />
+			</Hidden>
+			<Hidden smDown>
+				<ColorLine color="#e5e5e5" />
+			</Hidden>
+			<div className={classes.contactInfo}>
+				<Typography
+					variant="h6"
+					style={{
+						fontWeight: "bold",
+					}}
+				>
+					Group Information
+				</Typography>
+			</div>
+			<ColorLine color="#d6072b" />
+		</div>
+	);
 }
--- pages/group-page.js
+++ pages/group-page.js
@@ -10,26 +10,35 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 export default function GroupPage() {
-    return (
-        <React.Fragment>
-            <Head>
-                <title>Group name | AimforHD</title>
-                <meta
-                    name="viewport"
-                    content="minimum-scale=1, initial-scale=1, width=device-width"
-                />
-            </Head>
-            <Grid container justify="center">
-                <Grid item xs={11} md={3}>
-                    <LeftGroup />
-                </Grid>
-                <Grid item xs={11} md={5}>
-                    {/* <CenterProfile user={user} /> */}
-                </Grid>
-                <Grid item xs={false} md={4}>
-                    {/* suggested users - for later */}
-                </Grid>
-            </Grid>
-        </React.Fragment>
-    );
+	// const [group, setGroup] = useState({});
+	// useEffect(() => {
+	//     axios
+	//         .get(
+	//             "https://aim4hd.herokuapp.com/api/v1/users/5fab4912ffd1131f3cace694"
+	//         )
+	//         .then((res) => setUser(res.data.data.user))
+	//         .catch((err) => console.log(err));
+	// }, []);
+	return (
+		<React.Fragment>
+			<Head>
+				<title>Group name | AimforHD</title>
+				<meta
+					name="viewport"
+					content="minimum-scale=1, initial-scale=1, width=device-width"
+				/>
+			</Head>
+			<Grid container justify="center">
+				<Grid item xs={11} md={3}>
+					<LeftGroup />
+				</Grid>
+				<Grid item xs={11} md={5}>
+					{/* <CenterProfile user={user} /> */}
+				</Grid>
+				<Grid item xs={false} md={4}>
+					{/* suggested users - for later */}
+				</Grid>
+			</Grid>
+		</React.Fragment>
+	);
 }

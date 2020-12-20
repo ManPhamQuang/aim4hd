24ef9e561b87e1aa263c9064ff887c066b197419
Vu Thang <dangvuthang97@gmail.com>
2020-12-21 00:57:52 +0700

    Configure prettier to match group's style

--- pages/index.js
+++ pages/index.js
@@ -4,19 +4,19 @@ import Filter from "../components/Filter";
 import RecommendedUsers from "../components/RecommendUsers";
 
 export default function Home() {
-  return (
-    <Grid container justify="center">
-      <Grid item xs={12} md={3}>
-        <Filter />
-      </Grid>
-      <Grid item xs={11} md={5}>
-        <Posts />
-      </Grid>
-      <Hidden smDown>
-        <Grid item xs={false} md={4}>
-          <RecommendedUsers />
+    return (
+        <Grid container justify="center">
+            <Grid item xs={12} md={3}>
+                <Filter />
+            </Grid>
+            <Grid item xs={11} md={5}>
+                <Posts />
+            </Grid>
+            <Hidden smDown>
+                <Grid item xs={false} md={4}>
+                    <RecommendedUsers />
+                </Grid>
+            </Hidden>
         </Grid>
-      </Hidden>
-    </Grid>
-  );
+    );
 }
--- pages/sign-up.js
+++ pages/sign-up.js
@@ -14,279 +14,288 @@ import validator from "../utils/validator";
 import axios from "axios";
 import LoadingSpinner from "../components/LoadingSpinner";
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  root: {
-    zIndex: 10,
-  },
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    root: {
+        zIndex: 10,
+    },
 }));
 
 export default function SignIn({ courses, skills }) {
-  const classes = useStyles();
-  const auth = useContext(AuthContext);
-  const router = useRouter();
-  if (!auth.authData && typeof window !== "undefined") router.push("/");
-  if (auth.user && typeof window !== "undefined") router.push("/");
-  const [input, setInput] = useState({
-    name: auth.authData ? auth.authData.account.name : "",
-    school: "",
-    major: "",
-    description: "",
-    skills: [],
-    currentCourses: [],
-  });
+    const classes = useStyles();
+    const auth = useContext(AuthContext);
+    const router = useRouter();
+    if (!auth.authData && typeof window !== "undefined") router.push("/");
+    if (auth.user && typeof window !== "undefined") router.push("/");
+    const [input, setInput] = useState({
+        name: auth.authData ? auth.authData.account.name : "",
+        school: "",
+        major: "",
+        description: "",
+        skills: [],
+        currentCourses: [],
+    });
 
-  const [errorMsg, setErrorMsg] = useState({
-    school: "",
-    major: "",
-    skills: "",
-    currentCourses: "",
-  });
+    const [errorMsg, setErrorMsg] = useState({
+        school: "",
+        major: "",
+        skills: "",
+        currentCourses: "",
+    });
 
-  const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handleOnInputChange = event => {
-    if (
-      event.target.name === "skills" ||
-      event.target.name === "currentCourses"
-    ) {
-      setInput(input => ({
-        ...input,
-        [event.target.name]:
-          event.target.type === "checkbox"
-            ? event.target.checked
-            : event.target.value,
-      }));
-    }
-    setInput(input => ({
-      ...input,
-      [event.target.name]: event.target.value,
-    }));
-    let newErrorMsg = { ...errorMsg };
-    console.log(newErrorMsg);
-    const message = validator(event.target.name, event.target.value);
-    if (message) setErrorMsg({ ...errorMsg, [event.target.name]: message });
-    else {
-      delete newErrorMsg[event.target.name];
-      setErrorMsg(newErrorMsg);
-    }
-  };
-
-  const handleSubmitSignin = async event => {
-    event.preventDefault();
-    setIsLoading(true);
-    try {
-      const response = await axios.post(
-        "https://aim4hd.herokuapp.com/api/v1/users/signup",
-        {
-          ...input,
-          email: auth.authData.account.userName,
-          studentNumber: auth.authData.account.userName.split("@")[0],
-          microsoftUniqueId: auth.authData.uniqueId,
+    const handleOnInputChange = (event) => {
+        if (
+            event.target.name === "skills" ||
+            event.target.name === "currentCourses"
+        ) {
+            setInput((input) => ({
+                ...input,
+                [event.target.name]:
+                    event.target.type === "checkbox"
+                        ? event.target.checked
+                        : event.target.value,
+            }));
         }
-      );
-      if (`${response.status}`.startsWith("2")) {
-        setIsLoading(false);
-        console.log(response.data.data.user);
-        auth.login("signup", { user: response.data.data.user });
-        router.push("/");
-      }
-    } catch (error) {
-      setIsLoading(false);
-      console.log(error.response);
-    }
-  };
+        setInput((input) => ({
+            ...input,
+            [event.target.name]: event.target.value,
+        }));
+        let newErrorMsg = { ...errorMsg };
+        console.log(newErrorMsg);
+        const message = validator(event.target.name, event.target.value);
+        if (message) setErrorMsg({ ...errorMsg, [event.target.name]: message });
+        else {
+            delete newErrorMsg[event.target.name];
+            setErrorMsg(newErrorMsg);
+        }
+    };
 
-  return (
-    auth.authData && (
-      <Container component="main" maxWidth="xs">
-        {isLoading && <LoadingSpinner isLoading={isLoading} />}
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Avatar className={classes.avatar}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <form className={classes.form} onSubmit={handleSubmitSignin}>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              name="email"
-              label="Email Address"
-              disabled
-              value={auth.authData.account.userName}
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              label="Student Number"
-              name="studentNumber"
-              disabled
-              value={auth.authData.account.userName.split("@")[0]}
-            />
-            <TextField
-              color="secondary"
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="Name"
-              name="name"
-              value={input.name}
-              onChange={handleOnInputChange}
-            />
-            {errorMsg.name && (
-              <FormHelperText error={true}>{errorMsg.name}</FormHelperText>
-            )}
-            <TextField
-              color="secondary"
-              select
-              name="school"
-              name="school"
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              label="School"
-              SelectProps={{
-                value: input.school,
-                onChange: handleOnInputChange,
-              }}
-            >
-              {["SST", "SCD", "SBM"].map(school => (
-                <MenuItem value={school} key={school}>
-                  {school}
-                </MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              color="secondary"
-              required
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              label="Major"
-              name="major"
-              value={input.major}
-              onChange={handleOnInputChange}
-            />
-            {errorMsg.major && (
-              <FormHelperText error={true}>{errorMsg.major}</FormHelperText>
-            )}
-            <TextField
-              color="secondary"
-              multiline
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              label="Self Description"
-              name="description"
-              rows={4}
-              value={input.description}
-              onChange={handleOnInputChange}
-            />
-            <TextField
-              color="secondary"
-              select
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              label="Skills"
-              name="skills"
-              SelectProps={{
-                multiple: true,
-                value: input.skills,
-                onChange: handleOnInputChange,
-              }}
-            >
-              {skills.map(skill => (
-                <MenuItem key={skill.id} value={skill.id}>
-                  {skill.name}
-                </MenuItem>
-              ))}
-            </TextField>
-            {errorMsg.skills && (
-              <FormHelperText error={true}>{errorMsg.skills}</FormHelperText>
-            )}
-            <TextField
-              color="secondary"
-              select
-              variant="outlined"
-              margin="normal"
-              fullWidth
-              label="Current courses"
-              name="currentCourses"
-              SelectProps={{
-                multiple: true,
-                value: input.currentCourses,
-                onChange: handleOnInputChange,
-              }}
-            >
-              {courses.map(course => (
-                <MenuItem key={course.id} value={course.id}>
-                  {course.name}
-                </MenuItem>
-              ))}
-            </TextField>
-            {errorMsg.currentCourses && (
-              <FormHelperText error={true}>
-                {errorMsg.currentCourses}
-              </FormHelperText>
-            )}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-              className={classes.submit}
-              disabled={Object.keys(errorMsg).length > 0}
-            >
-              {isLoading ? "Processing..." : "Sign In"}
-            </Button>
-          </form>
-        </div>
-      </Container>
-    )
-  );
+    const handleSubmitSignin = async (event) => {
+        event.preventDefault();
+        setIsLoading(true);
+        try {
+            const response = await axios.post(
+                "https://aim4hd.herokuapp.com/api/v1/users/signup",
+                {
+                    ...input,
+                    email: auth.authData.account.userName,
+                    studentNumber: auth.authData.account.userName.split("@")[0],
+                    microsoftUniqueId: auth.authData.uniqueId,
+                }
+            );
+            if (`${response.status}`.startsWith("2")) {
+                setIsLoading(false);
+                console.log(response.data.data.user);
+                auth.login("signup", { user: response.data.data.user });
+                router.push("/");
+            }
+        } catch (error) {
+            setIsLoading(false);
+            console.log(error.response);
+        }
+    };
+
+    return (
+        auth.authData && (
+            <Container component="main" maxWidth="xs">
+                {isLoading && <LoadingSpinner isLoading={isLoading} />}
+                <CssBaseline />
+                <div className={classes.paper}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <form
+                        className={classes.form}
+                        onSubmit={handleSubmitSignin}
+                    >
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            name="email"
+                            label="Email Address"
+                            disabled
+                            value={auth.authData.account.userName}
+                        />
+                        <TextField
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            label="Student Number"
+                            name="studentNumber"
+                            disabled
+                            value={auth.authData.account.userName.split("@")[0]}
+                        />
+                        <TextField
+                            color="secondary"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="Name"
+                            name="name"
+                            value={input.name}
+                            onChange={handleOnInputChange}
+                        />
+                        {errorMsg.name && (
+                            <FormHelperText error={true}>
+                                {errorMsg.name}
+                            </FormHelperText>
+                        )}
+                        <TextField
+                            color="secondary"
+                            select
+                            name="school"
+                            name="school"
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            label="School"
+                            SelectProps={{
+                                value: input.school,
+                                onChange: handleOnInputChange,
+                            }}
+                        >
+                            {["SST", "SCD", "SBM"].map((school) => (
+                                <MenuItem value={school} key={school}>
+                                    {school}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        <TextField
+                            color="secondary"
+                            required
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            label="Major"
+                            name="major"
+                            value={input.major}
+                            onChange={handleOnInputChange}
+                        />
+                        {errorMsg.major && (
+                            <FormHelperText error={true}>
+                                {errorMsg.major}
+                            </FormHelperText>
+                        )}
+                        <TextField
+                            color="secondary"
+                            multiline
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            label="Self Description"
+                            name="description"
+                            rows={4}
+                            value={input.description}
+                            onChange={handleOnInputChange}
+                        />
+                        <TextField
+                            color="secondary"
+                            select
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            label="Skills"
+                            name="skills"
+                            SelectProps={{
+                                multiple: true,
+                                value: input.skills,
+                                onChange: handleOnInputChange,
+                            }}
+                        >
+                            {skills.map((skill) => (
+                                <MenuItem key={skill.id} value={skill.id}>
+                                    {skill.name}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        {errorMsg.skills && (
+                            <FormHelperText error={true}>
+                                {errorMsg.skills}
+                            </FormHelperText>
+                        )}
+                        <TextField
+                            color="secondary"
+                            select
+                            variant="outlined"
+                            margin="normal"
+                            fullWidth
+                            label="Current courses"
+                            name="currentCourses"
+                            SelectProps={{
+                                multiple: true,
+                                value: input.currentCourses,
+                                onChange: handleOnInputChange,
+                            }}
+                        >
+                            {courses.map((course) => (
+                                <MenuItem key={course.id} value={course.id}>
+                                    {course.name}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                        {errorMsg.currentCourses && (
+                            <FormHelperText error={true}>
+                                {errorMsg.currentCourses}
+                            </FormHelperText>
+                        )}
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            color="primary"
+                            className={classes.submit}
+                            disabled={Object.keys(errorMsg).length > 0}
+                        >
+                            {isLoading ? "Processing..." : "Sign In"}
+                        </Button>
+                    </form>
+                </div>
+            </Container>
+        )
+    );
 }
 
 export async function getStaticProps(context) {
-  const skillsResponse = axios.get(
-    "https://aim4hd.herokuapp.com/api/v1/skills"
-  );
-  const coursesResponse = axios.get(
-    "https://aim4hd.herokuapp.com/api/v1/courses?limit=100"
-  );
-  try {
-    const result = await Promise.all([skillsResponse, coursesResponse]);
-    console.log(result);
-    return {
-      props: {
-        skills: result[0].data.skills,
-        courses: result[1].data.data.courses,
-      },
-    };
-  } catch (error) {
-    console.log(error);
-  }
+    const skillsResponse = axios.get(
+        "https://aim4hd.herokuapp.com/api/v1/skills"
+    );
+    const coursesResponse = axios.get(
+        "https://aim4hd.herokuapp.com/api/v1/courses?limit=100"
+    );
+    try {
+        const result = await Promise.all([skillsResponse, coursesResponse]);
+        console.log(result);
+        return {
+            props: {
+                skills: result[0].data.skills,
+                courses: result[1].data.data.courses,
+            },
+        };
+    } catch (error) {
+        console.log(error);
+    }
 }

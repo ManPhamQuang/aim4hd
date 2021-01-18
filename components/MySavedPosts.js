b91e3dec800213d26c6129281b195ddc2593a615
Vu Thang <dangvuthang97@gmail.com>
2021-01-18 22:01:25 +0700

    Render PostCard when user navigate to his/her saved posts page

--- components/MySavedPosts.js
+++ components/MySavedPosts.js
@@ -1,5 +1,10 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { makeStyles } from "@material-ui/core/styles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import PostCard from "./PostCard";
+import { Typography } from "@material-ui/core";
+
 const getInterestedPosts = async (id) => {
     try {
         const response = await axios.get(
@@ -13,14 +18,29 @@ const getInterestedPosts = async (id) => {
     }
 };
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        width: "100%",
+        marginTop: "12px",
+    },
+    progress: {
+        display: "flex",
+        justifyContent: "center",
+    },
+}));
+
 const MySavedPosts = ({ user }) => {
     const [interestedPosts, setInterestedPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const classes = useStyles();
 
     useEffect(() => {
         if (!user) return;
         const getPostsData = async () => {
+            setIsLoading(true);
             try {
                 const data = await getInterestedPosts(user.id);
+                setIsLoading(false);
                 setInterestedPosts(data);
             } catch (error) {
                 console.log(error);
@@ -29,7 +49,20 @@ const MySavedPosts = ({ user }) => {
         getPostsData();
     }, [user]);
     console.log(interestedPosts);
-    return "123";
+    return (
+        <div className={classes.root}>
+            {isLoading && (
+                <div className={classes.progress}>
+                    <CircularProgress />
+                </div>
+            )}
+            {!isLoading &&
+                interestedPosts.length > 0 &&
+                interestedPosts.map((post) => (
+                    <PostCard key={post.id} {...post} />
+                ))}
+        </div>
+    );
 };
 
 export default MySavedPosts;

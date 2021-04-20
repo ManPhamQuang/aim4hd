8c9b3ecfe54d4a05467d71d78643323ba39b3fee
Man Pham <s3804811@rmit.edu.vn>
2021-04-20 17:55:04 +0700

    test receive new notificaiton successfully

--- pages/testSocket.js
+++ pages/testSocket.js
@@ -1,9 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import io, { Socket } from "socket.io-client";
+import AuthContext from "../utils/authContext";
 
 // const socket = io.connect("http://localhost:4000");
 const endpoint = "http://localhost:4000";
-const socket = io(endpoint);
 
 function useSocket(url) {
     const [socket, setSocket] = useState(null);
@@ -26,11 +26,51 @@ function useSocket(url) {
 }
 
 function SocketTest() {
-    const [res, setRes] = useState("");
+    const [res, setRes] = useState([]);
+    const auth = useContext(AuthContext);
+    // socket.on("notifications", ({ notifications }) => {
+    //     console.log("getting messages");
+    //     console.log(notifications);
+    //     setRes(notifications);
+    // });
     useEffect(() => {
-        socket.on("message", (data) => console.log(data));
+        const socket = io(endpoint);
+        socket.emit("getNotification", { id: "606a6fb9b824bd0015da77a7" });
+        socket.on(
+            "notifications",
+            ({ notifications }) => {
+                console.log("getting messages");
+                console.log(notifications);
+                setRes(notifications);
+                // clean up when unmount
+                return () => io.disconnect();
+            },
+            []
+        );
+        socket.on("newNoti", (data) => {
+            console.log("new noti!!!!");
+            console.log(data);
+            setRes([data, ...res]);
+        });
+        // if (auth.user) {
+        //     console.log("ran with user id: " + auth.user.id);
+        //     socket.emit("getNotification", { id: auth.user?._id }); // somehow auth is not ready when emitting getNotification
+        // }
+        // return socket.close();
     }, []);
-    return <h1>heel</h1>;
+
+    return (
+        auth.user && (
+            <div>
+                <h1>hello2</h1>
+                <h1>{auth.user.name}</h1>
+                {res.map((noti, idx) => (
+                    <h1 key={noti._id}>{noti.id}</h1>
+                ))}
+            </div>
+        )
+    );
+    // change some render thing for id to pass TOTO: FIX THIS SHIT LATER
 }
 
 export default SocketTest;

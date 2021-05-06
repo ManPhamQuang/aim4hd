3638085be945d84b46c213d506fa73c38f862b36
NguyenDangLamPhuong <s3754105@rmit.edu.vn>
2021-05-06 13:20:36 +0700

    update

--- pages/landingpage.js
+++ pages/landingpage.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    breaker: {
+        backgroundColor: "#f2f2f2",
+        width: "100%",
+        height: "3px",
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(1),
+    },
+}));
 
 export default function LandingPage() {
-    return <h1>landing page</h1>;
+    const classes = useStyles();
+    // return <h1>landing page</h1>;
+    return (
+        <h1>landing page</h1>
+
+    );
 }

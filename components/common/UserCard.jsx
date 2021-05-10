1b3a14a7362c8b6d839288108f56972321f6ce42
Nguyễn Hoàng Thiên Ân <57244622+s3825455@users.noreply.github.com>
2021-05-10 14:43:37 +0700

    user search

--- components/common/UserCard.jsx
+++ components/common/UserCard.jsx
@@ -8,15 +8,20 @@ import Typography from "@material-ui/core/Typography";
 import { Avatar, Container, Grid, CardHeader } from "@material-ui/core";
 import Link from "next/link";
 import { withSnackbar } from "notistack";
-import ChatIcon from "@material-ui/icons/Chat";
 import IconButton from "@material-ui/core/IconButton";
+import EmailIcon from "@material-ui/icons/Email";
+import CardActions from "@material-ui/core/CardActions";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import clsx from "clsx";
+import Chip from "@material-ui/core/Chip";
+import Divider from "@material-ui/core/Divider";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        display: "flex",
         backgroundColor: "#FFFFFF",
-        // ...theme.userCard,
-        minHeight: 120,
+        // minHeight: 90,
+        // maxHeight: 90,
         marginTop: "1rem",
         boxShadow: "none",
         "&:hover": {
@@ -33,11 +38,11 @@ const useStyles = makeStyles((theme) => ({
     },
 
     cover: {
-        width: theme.spacing(15),
-        height: theme.spacing(15),
+        width: theme.spacing(10),
+        height: theme.spacing(10),
         [theme.breakpoints.down("md")]: {
-            width: theme.spacing(10),
-            height: theme.spacing(10),
+            width: theme.spacing(8),
+            height: theme.spacing(8),
         },
         borderRadius: "7px",
         // width: "100%",
@@ -51,52 +56,149 @@ const useStyles = makeStyles((theme) => ({
             textDecoration: "underline",
         },
     },
-    icon: {
-        display: "flex",
-        alignItems: "flex-end",
+    // icon: {
+    //     marginLeft: "1rem",
+    //     color: "gray",
+    // },
+
+    expand: {
+        transform: "rotate(0deg)",
+        marginLeft: "auto",
+        transition: theme.transitions.create("transform", {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: "rotate(180deg)",
+    },
+    action: {
+        alignSelf: "center",
+    },
+    outsideDiv: {
+        border: "2px solid black",
+        borderRadius: "1rem",
+        padding: "1rem",
+    },
+    chipTest: {
+        marginRight: "0.5rem",
+        overflow: "hidden",
+        marginBottom: "1rem",
     },
 }));
 
 export default function UserCard({ user }) {
-    const { avatar, major, school, name } = user;
+    const { avatar, major, school, name, description } = user;
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return (
-        <Link href={`/users/${user._id}`}>
-            <a
-                style={{
-                    color: "inherit",
-                    textDecoration: "none",
-                }}
-            >
-                <Card className={classes.root}>
-                    <CardHeader
-                        classes={{ root: classes.cardHeaderRoot }}
-                        avatar={
-                            <Avatar src={avatar} className={classes.cover} />
-                        }
-                        title={
-                            <Link href={`/users/${user._id}`}>
-                                <a className={classes.hover}>{name}</a>
-                            </Link>
-                        }
-                        titleTypographyProps={{
-                            variant: "h6",
-                            align: "left",
+        <div>
+            <Card className={classes.root}>
+                <CardHeader
+                    classes={{
+                        root: classes.cardHeaderRoot,
+                        action: classes.action,
+                    }}
+                    avatar={<Avatar src={avatar} className={classes.cover} />}
+                    title={
+                        <Link href={`/users/${user._id}`}>
+                            <a className={classes.hover}>{name}</a>
+                        </Link>
+                    }
+                    titleTypographyProps={{
+                        variant: "h6",
+                        align: "left",
+                    }}
+                    subheader={`${school} - ${major}`}
+                    subheaderTypographyProps={{
+                        align: "left",
+                        variant: "body1",
+                    }}
+                />
+                <CardContent>
+                    <Typography
+                        variant="body1"
+                        style={{
+                            marginBottom: "0.5rem",
+                            fontWeight: "500",
                         }}
-                        subheader={`${school} - ${major}`}
-                        subheaderTypographyProps={{
-                            align: "left",
-                            variant: "body1",
-                            display: "block",
-                        }}
-                        // action={
-                        //     <IconButton className={classes.icon}>
-                        //         <ChatIcon />
-                        //     </IconButton>
-                        // }
-                    />
-                </Card>
-            </a>
-        </Link>
+                    >
+                        {description}
+                    </Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <div className={classes.outsideDiv}>
+                            <div>
+                                <Typography
+                                    variant="body1"
+                                    style={{
+                                        marginBottom: "0.5rem",
+                                        fontWeight: "500",
+                                    }}
+                                >
+                                    Skills
+                                </Typography>
+                            </div>
+                            <div>
+                                {user.skills
+                                    ? user.skills.map((skill, idx) => {
+                                          return (
+                                              <Chip
+                                                  key={idx}
+                                                  className={classes.chipTest}
+                                                  clickable
+                                                  label={skill.name}
+                                              />
+                                          );
+                                      })
+                                    : null}
+                            </div>
+                            <Divider variant="middle" />
+                            <div>
+                                <Typography
+                                    variant="body1"
+                                    style={{
+                                        marginBottom: "0.5rem",
+                                        fontWeight: "500",
+                                    }}
+                                >
+                                    Current Courses
+                                </Typography>
+                            </div>
+                            <div>
+                                {user.currentCourses
+                                    ? user.currentCourses.map((course, idx) => {
+                                          return (
+                                              <Chip
+                                                  key={idx}
+                                                  className={classes.chipTest}
+                                                  label={course.name}
+                                                  clickable
+                                              />
+                                          );
+                                      })
+                                    : null}
+                            </div>
+                        </div>
+                    </CardContent>
+                </Collapse>
+            </Card>
+        </div>
     );
 }
